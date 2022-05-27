b61b13d2fc0960b74c607b57343711b7031194d5
David Hargitai <david.hargitai@borngroup.com>
2022-05-27 18:11:26 +0200

    wip home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        'webflow-icons': ["webflow-icons"],
+      },
+    },
   },
   plugins: [],
 };
